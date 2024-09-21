f4d5cd68311c7f750e0139ef888386c44e74a4d9
kraraead28 <kraraead206@gmail.com>
2024-09-21 06:09:21 +0300

    Uploaded Successfully

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, idle
+from pyromod import listen
+from casery import caes, casery, group, source, photosource, caserid, ch, bot_token, bot_token2
+
+bot = Client("CAR", api_id=26171594, api_hash="eed6ccae7257c92043a593be91bd45bf", bot_token=bot_token, plugins=dict(root="CASER"))
+lolo = Client("hossam", api_id=26171594, api_hash="eed6ccae7257c92043a593be91bd45bf", session_string=bot_token2)    
+
+DEVS = caes
+DEVSs = []
+bot_id = bot.bot_token.split(":")[0]
+
+async def start_zombiebot():
+    print("تم تشغيل الصانع بنجاح..💗")
+    await bot.start()
+    await lolo.start()
+    try:
+      await bot.send_message(casery, "**تم تشغيل الصانع بنجاح..💗**")
+    except:
+      pass
+    await idle()
