b2dad47b7a63df5eb1aaf005194d339a5fc1535c
kraraead28 <kraraead206@gmail.com>
2024-09-21 06:09:20 +0300

    Uploaded Successfully

---
+++ casery.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["zzkzv"]
+casery = "zzkzv"
+##ايدي مطور السورس
+caserid = 5485199669
+##قناه السورس
+source = "https://t.me/Files_php3" 
+ch = "Files_php3" 
+##جروب السورس
+group = "https://t.me/Files_php3" 
+##لوجو السورس
+photosource = "https://telegra.ph/file/79d346793ec3e73d5e9f0.jpg"
+##توكن الصانع
+bot_token="7700478001:AAEMoh6r1k3kRLXY50vUoxuMQfhSQP6FfMI"
+##جلسه الاشتراك
+bot_token2="BAGB5kgAUt0Ig-quVb0LQ8aZTHefrO-lNbvEsEZCrcT526zXyASlEv4stry_AQHWHxxT19_yEEnbWEm9qjBzMvvlcgSP7xw75gQvxR4rZPIfs7LoEPfcKO39VDiaK-qr9mj51k0IMZLBMkuISpTBim6dkXx5zm1XkGBvnSu_JfdbCC7343lkdvQfsm2M0K4fjjtmi5oxwzzNv_0F8hI-nRYl8lS3wTqv8SWVEQ5JCROs84xtASPAfIfuskjWPferORA695wL8JzbnbeOUOR1WbD6z50nWzfUPJxLh9rfKVGrbgBaAgjfKlIk5C4jDARaw6ZYSx3PeMTmgdtDuYO57sqfpoLJmAAAAAFG8YE1AA"
